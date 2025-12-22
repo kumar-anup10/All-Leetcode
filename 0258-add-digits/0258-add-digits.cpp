17ee913c7da19b8feb4b617b7df220a30c1e0b7e
Kumar Anup <kumaranup9135072766@gmail.com>
2025-12-22 21:39:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (77.37%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int addDigits(int num) 
+    {
+        int sum,rem;
+        while(num>9)
+        {
+            sum=0;
+            while(num!=0)
+            {
+                rem=num%10;
+                sum=sum+rem;
+                num=num/10;
+            }
+            num=sum;
+        }
+        return num;
+    }
+    
+};

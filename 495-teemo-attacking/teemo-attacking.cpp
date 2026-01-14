693bd571b554325829a0269fd3e4f7b57a375034
Kumar Anup <kumaranup9135072766@gmail.com>
2026-01-14 22:20:13 +0530

    Time: 0 ms (100.00%) | Memory: 29.9 MB (27.32%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int total = 0;
+        int n = timeSeries.size();
+        
+        for(int i = 0; i < n - 1; i++) {
+            total += min(duration, timeSeries[i+1] - timeSeries[i]);
+        }
+        
+        return total + duration;
+    }
+};

57b26582e754900193a27e5b23731b60d4afc34f
Kumar Anup <kumaranup9135072766@gmail.com>
2026-01-20 22:18:23 +0530

    Time: 4 ms (4.37%) | Memory: 14 MB (81.11%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int ans=0;
+        for(int i=0;i<logs.size();i++)
+        {
+            if(logs[i]=="./")
+            continue;
+            else if(logs[i]=="../")
+            {
+                if(ans>0)
+                ans--;
+            }
+            else 
+            ans++;
+        }
+        return ans;
+    }
+};

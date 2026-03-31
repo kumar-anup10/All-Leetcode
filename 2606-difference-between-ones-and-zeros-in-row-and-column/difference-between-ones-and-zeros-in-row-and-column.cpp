e2507958767ed7196629774f96df647526caf7ab
Kumar Anup <kumaranup9135072766@gmail.com>
2026-03-31 15:32:27 +0530

    Time: 29 ms (72.56%) | Memory: 121.7 MB (74.10%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -3,48 +3,28 @@ public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
 
-        vector<vector<int>> v1r(n, vector<int>(m)), v0r(n, vector<int>(m));
+        vector<int> row1(n, 0), col1(m, 0);
 
+        // Count 1s in rows and columns
         for(int i = 0; i < n; i++) {
-            int count0 = 0, count1 = 0;
-
             for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 0) count0++;
-                if(grid[i][j] == 1)count1++;
-            }
-
-            for(int j = 0; j < m; j++) {
-                v1r[i][j] = count1;
-                v0r[i][j] = count0;
+                if(grid[i][j] == 1) {
+                    row1[i]++;
+                    col1[j]++;
+                }
             }
         }
 
-        vector<vector<int>> v1c(n, vector<int>(m)), v0c(n, vector<int>(m));
+        vector<vector<int>> ans(n, vector<int>(m));
 
-        for(int j = 0; j < m; j++) {
-            int count0 = 0, count1 = 0;
-
-            for(int i = 0; i < n; i++) {
-                if(grid[i][j] == 0) count0++;
-                if(grid[i][j] == 1) count1++;
-            }
-
-            for(int i = 0; i < n; i++) {
-                v1c[i][j] = count1;
-                v0c[i][j] = count0;
-            }
-        }
-
-        vector<vector<int>> ans;
-
+        // Compute result
         for(int i = 0; i < n; i++) {
-            vector<int> temp(m);
-
             for(int j = 0; j < m; j++) {
-                temp[j] = v1r[i][j] + v1c[i][j] - v0r[i][j] - v0c[i][j];
-            }
+                int row0 = m - row1[i];
+                int col0 = n - col1[j];
 
-            ans.push_back(temp);
+                ans[i][j] = row1[i] + col1[j] - row0 - col0;
+            }
         }
 
         return ans;
